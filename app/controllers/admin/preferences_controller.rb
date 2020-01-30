acbd05ca8bb9a0b8a1ff8e5d7d1a9ed8aad7626d
Alouiscious Media <info@reneesoul.com>
2020-01-30 06:55:11 -0800

    preferences module creation

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,8 @@
+class Admin::PreferencesController < ApplicationController
+  def index
+    # @song_count = Song.count
+    # @artist_count = Artist.count
+    # @last_song = Song.last
+    # @last_artist = Artist.last
+  end
+end
